f176ad1588783895e40a3982dd0186bc71733b80
Fran <frandioniz@gmail.com>
2017-06-05 08:44:58 +0100

    First commit

---
+++ app/src/main/kotlin/com/diodev/kotlinsample/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.diodev.kotlinsample
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener(View.OnClickListener { view ->
+            Snackbar.make(view, "Hello Kotlin", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        })
+    }
+
+}
